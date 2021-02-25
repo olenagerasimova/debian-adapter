603530c9c27c80a43c842c4ebf2028892addc774
Alena <olena.gerasimova@gmail.com>
2021-02-25 13:22:34 +0300

    #41 - update Release index
    PR: #48

--- src/test/java/com/artipie/debian/metadata/ReleaseAstoTest.java
+++ src/test/java/com/artipie/debian/metadata/ReleaseAstoTest.java
@@ -31,8 +31,12 @@ import com.artipie.asto.ext.PublisherAs;
 import com.artipie.asto.memory.InMemoryStorage;
 import com.artipie.debian.Config;
 import com.artipie.http.slice.KeyFromPath;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
+import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream;
 import org.cactoos.list.ListOf;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
@@ -100,19 +104,19 @@ class ReleaseAstoTest {
     }
 
     @Test
-    void addsNewRecord() {
+    void addsNewRecord() throws IOException {
         this.asto.save(
-            new Key.From("dists/my-deb/main/binaty-amd64/Packages.gz"), Content.EMPTY
+            new Key.From("dists/my-deb/main/binary-amd64/Packages.gz"), Content.EMPTY
         ).join();
-        final Key key = new Key.From("dists/my-deb/main/binaty-intel/Packages.gz");
-        this.asto.save(key, Content.EMPTY).join();
+        final Key key = new Key.From("dists/my-deb/main/binary-intel/Packages.gz");
+        this.asto.save(key, new Content.From(this.packed("abc123".getBytes()))).join();
         final ListOf<String> content = new ListOf<>(
             "Codename: my-deb",
             "Architectures: amd64 intel",
             "Components: main",
             "Date:",
             "SHA256:",
-            " abc123 main/binaty-amd64/Packages.gz"
+            " abc123 2 main/binaty-amd64/Packages.gz"
         );
         this.asto.save(
             new Key.From("dists/my-deb/Release"),
@@ -130,26 +134,28 @@ class ReleaseAstoTest {
                 .asciiString().toCompletableFuture().join(),
             Matchers.allOf(
                 new StringContainsInOrder(content),
-                new StringContains("main/binaty-intel/Packages.gz")
+                // @checkstyle LineLengthCheck (3 lines)
+                new StringContains(" 9751b63dcb589f0d84d20dcf5a0d347939c6f4f09d7911c40f330bfe6ffe686e 26 main/binary-intel/Packages.gz"),
+                new StringContains(" 6ca13d52ca70c883e0f0bb101e425a89e8624de51db2d2392593af6a84118090 6 main/binary-intel/Packages")
             )
         );
     }
 
     @Test
-    void updatesRecord() {
+    void updatesRecordInTheMiddle() throws IOException {
         this.asto.save(
-            new Key.From("dists/my-repo/main/binaty-amd64/Packages.gz"), Content.EMPTY
+            new Key.From("dists/my-repo/main/binary-amd64/Packages.gz"), Content.EMPTY
         ).join();
-        final Key key = new Key.From("dists/my-repo/main/binaty-intel/Packages.gz");
-        this.asto.save(key, Content.EMPTY).join();
+        final Key key = new Key.From("dists/my-repo/main/binary-intel/Packages.gz");
+        this.asto.save(key, new Content.From(this.packed("xyz".getBytes()))).join();
         final ListOf<String> content = new ListOf<>(
             "Codename: my-repo",
             "Architectures: amd64 intel",
             "Components: main",
             "Date:",
             "SHA256:",
-            " xyz098 main/binaty-intel/Packages.gz",
-            " abc123 main/binaty-amd64/Packages.gz"
+            " xyz098 2 main/binary-intel/Packages.gz",
+            " abc123 4 main/binary-amd64/Packages.gz"
         );
         this.asto.save(
             new Key.From("dists/my-repo/Release"),
@@ -162,11 +168,50 @@ class ReleaseAstoTest {
                 Optional.of(Yaml.createYamlMappingBuilder().build())
             )
         ).update(key).toCompletableFuture().join();
-        // @checkstyle LineLengthCheck (2 lines)
+        // @checkstyle LineLengthCheck (3 lines)
         // @checkstyle MagicNumberCheck (1 line)
-        content.set(5, " e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855 main/binaty-intel/Packages.gz");
+        content.set(5, " eca44f5be15c27f009b837cf98df6a359304e868f024cfaff7f139baa6768d16 23 main/binary-intel/Packages.gz");
+        content.add(" 3608bca1e44ea6c4d268eb6db02260269892c0b42b86bbf1e77a6fa16c3c9282 3 main/binary-intel/Packages");
         MatcherAssert.assertThat(
             new PublisherAs(this.asto.value(new KeyFromPath("dists/my-repo/Release")).join())
+                .asciiString().toCompletableFuture().join(),
+            new IsEqual<>(String.join("\n", content))
+        );
+    }
+
+    @Test
+    void updatesRecordAtTheEnd() throws IOException {
+        this.asto.save(
+            new Key.From("dists/deb-test/main/binary-amd64/Packages.gz"), Content.EMPTY
+        ).join();
+        final Key key = new Key.From("dists/deb-test/main/binary-intel/Packages.gz");
+        this.asto.save(key, new Content.From(this.packed("098".getBytes()))).join();
+        final ListOf<String> content = new ListOf<>(
+            "Codename: deb-test",
+            "Architectures: amd64 intel",
+            "Components: main",
+            "Date:",
+            "SHA256:",
+            " xyz098 2 main/binary-amd64/Packages.gz",
+            " abc123 4 main/binary-intel/Packages.gz"
+        );
+        this.asto.save(
+            new Key.From("dists/deb-test/Release"),
+            new Content.From(String.join("\n", content).getBytes(StandardCharsets.UTF_8))
+        ).join();
+        new Release.Asto(
+            this.asto,
+            new Config.FromYaml(
+                "deb-test",
+                Optional.of(Yaml.createYamlMappingBuilder().build())
+            )
+        ).update(key).toCompletableFuture().join();
+        // @checkstyle LineLengthCheck (3 lines)
+        // @checkstyle MagicNumberCheck (1 line)
+        content.set(6, " 4a82f377b30e07bc43f712d4e5ac4783b9e53de23980753e121618357be09c3c 23 main/binary-intel/Packages.gz");
+        content.add(" 35e1d1aeed3f7179b02a0dfde8f4e826e191649ee2acfd6da6b2ce7a12aa0f8b 3 main/binary-intel/Packages");
+        MatcherAssert.assertThat(
+            new PublisherAs(this.asto.value(new KeyFromPath("dists/deb-test/Release")).join())
                 .asciiString().toCompletableFuture().join(),
             new IsEqual<>(String.join("\n", content))
         );
@@ -184,5 +229,14 @@ class ReleaseAstoTest {
             ).key(),
             new IsEqual<>(new Key.From("dists/deb-repo/Release"))
         );
+    }
+
+    private byte[] packed(final byte[] bytes) throws IOException {
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try (GzipCompressorOutputStream gcos =
+            new GzipCompressorOutputStream(new BufferedOutputStream(baos))) {
+            gcos.write(bytes);
+        }
+        return baos.toByteArray();
     }
 }
